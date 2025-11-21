8f232816274504b003158be09c778d85af03f1dc
rajankumar-dev <rk1620703@gmail.com>
2025-11-21 21:55:41 +0530

    order Schema

---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,9 @@
+const { Schema } = require("mongoose");
+
+const OrdersSchema = new Schema({
+    name: String,
+    qty: Number,
+    price: Number,
+    model: String,
+});
+module.exports = { OrdersSchema }
