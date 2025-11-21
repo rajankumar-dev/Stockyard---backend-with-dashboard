8f706fb357f07f4918b5a66df53a21b5db50de49
rajankumar-dev <rk1620703@gmail.com>
2025-11-21 23:02:24 +0530

    addTemp position

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const {HoldingsModel} = require('./model/HoldingsModel')
+const {HoldingsModel} = require('./model/HoldingsModel');
+const {PositionsModel} = require('./model/PositionsModel');
 
 
 const PORT = process.env.PORT || 3002;
@@ -134,6 +135,44 @@ const app = express();
 // res.send("Done!")
 // });
 
+// app.get("/addPositions", async (req, res) => {
+//     let temPositions = [
+//   {
+//     product: "CNC",
+//     name: "EVEREADY",
+//     qty: 2,
+//     avg: 316.27,
+//     price: 312.35,
+//     net: "+0.58%",
+//     day: "-1.24%",
+//     isLoss: true,
+//   },
+//   {
+//     product: "CNC",
+//     name: "JUBLFOOD",
+//     qty: 1,
+//     avg: 3124.75,
+//     price: 3082.65,
+//     net: "+10.04%",
+//     day: "-1.35%",
+//     isLoss: true,
+//   },
+// ];
+// temPositions.forEach((item) => {
+//     let newPosition = new PositionsModel({
+//         product: item.product,
+//         name: item.name,
+//         qty: item.qty,
+//         avg: item.avg,
+//         price: item.price,
+//         net: item.net,
+//         day: item.day,
+//         isLoss: item.isLoss,
+//     });
+//     newPosition.save();
+// });
+// res.send("done!");
+// });
 
 app.listen(PORT, () => {
     console.log("app is started");
