80afe8ef6adbb3272d539583a747c44f0c34b9cd
rajankumar-dev <rk1620703@gmail.com>
2025-11-22 10:17:34 +0530

    complete new order functionality

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const { HoldingsModel } = require('./model/HoldingsModel');
 const { PositionsModel } = require('./model/PositionsModel');
+const { OrdersModel } = require('./model/OrdersModel');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3002;
@@ -226,6 +227,18 @@ app.get("/allHoldings", async (req, res) => {
 app.get("/allPositions", async (req, res) => {
     let allPositions=await PositionsModel.find({});
     res.json(allPositions);
+});
+
+app.post("/newOrder", async(req, res)=> {
+    let newOrder = new OrdersModel({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
+
+    newOrder.save();
+    res.send("Order Saved!");
 });
 
 app.listen(PORT, () => {
